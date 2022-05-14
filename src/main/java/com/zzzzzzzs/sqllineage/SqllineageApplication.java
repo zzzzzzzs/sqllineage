243b5d1672d8e02be090a92787052eef10a64136
zzzzzzzs <1443539042@qq.com>
2022-05-15 00:08:42 +0800

    init project

---
+++ src/main/java/com/zzzzzzzs/sqllineage/SqllineageApplication.java
@@ -0,0 +1,12 @@
+package com.zzzzzzzs.sqllineage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqllineageApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SqllineageApplication.class, args);
+  }
+}
---
+++ src/test/java/com/zzzzzzzs/sqllineage/SqllineageApplicationTests.java
@@ -0,0 +1,11 @@
+package com.zzzzzzzs.sqllineage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqllineageApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
