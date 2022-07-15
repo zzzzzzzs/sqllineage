65fe5f5b866d8f63766770552f254e7d16424156
赵硕 <1443539042@qq.com>
2022-07-15 14:50:52 +0800

    更改端口

--- src/main/java/com/zzzzzzzs/sqllineage/SqllineageApplication.java
+++ src/main/java/com/zzzzzzzs/sqllineage/SqllineageApplication.java
@@ -2,8 +2,8 @@ package com.zzzzzzzs.sqllineage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// http://localhost:9000/api/swagger-ui.html#/
-// http://localhost:9000/api/doc.html#/
+// http://localhost:10001/api/swagger-ui.html#/
+// http://localhost:10001/api/doc.html#/
 @SpringBootApplication
 public class SqllineageApplication {
 
